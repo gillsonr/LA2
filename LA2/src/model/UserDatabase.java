14a59dc8199d0dae65ea8e16329cc5e497d59858
runkle-austin <166458209+runkle-austin@users.noreply.github.com>
2025-03-17 16:15:50 -0700

    Add files via upload

--- LA2/src/model/UserDatabase.java
+++ LA2/src/model/UserDatabase.java
@@ -3,52 +3,40 @@ package model;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-import java.util.ArrayList;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
 import java.util.HashMap;
 
 public class UserDatabase {
-
-	// TODO no constructor
 	
-	// TODO change users to hashmap with username as key and (salted and hashed password, userdata) as value (tuple)
-	// or something like that, makes searching easier
-	
-	
-	/*
-	 * I dont understand what this is working towards. Is it the data of users that have already 
-	 * been made and are being accessed from a text file that we made? The accountInfo[userNum] doesnt make sense
-	 * with your outline of the file being "username|password|songs", in this format each new line should be a new user.
-	 * User number as a index in a single line isn't clicking in my head.
-	 * 
-	 * RN it seems like this is creating users from a text file. But the users should already be made right?
-	 * If it is correct ^, should we be remaking everything each time? that seems inefficient but not sure 
-	 * how else to do it
-	 * 
-	 * If it is the user data, should we have separate files for each user or just one big file
-	 */
-	
+	// hashmap with user name as the key and User as the value
 	private HashMap<String, User> users = new HashMap<String, User>();
+	
 	// load the current users in the file Users.txt
-	// this file will be formated "username|password|song,song"
+
 	public void loadDatabase() throws FileNotFoundException {
 		
-		// loads album file
+		// this file will be formated "userName,password,salt"
 		File fr = new File("Users.txt");
 		Scanner scanner = new Scanner(fr);
-		int userNum = 0;
 		
 		while(scanner.hasNextLine()) {
 			String line = scanner.nextLine();
-			String[] accountInfo = line.split("|");
-			// create a user an add it to the user list
+			String[] accountInfo = line.split(",");
+			// create a user an add it to the user map
+			User curUser = new User(accountInfo[0], accountInfo[1]);
 			
-			// TODO this uses the same exact things for both parameters
-			User curUser = new User(accountInfo[userNum], accountInfo[userNum]);
-			users.put(accountInfo[userNum],curUser);
+			//TODO convert the salt (hex) into byte[]
+			byte[] salt = new byte[64];
+			
+			curUser.setSalt(salt);
+			
+			// add the user to the map of users
+			users.put(accountInfo[0], curUser);
 			
 			// TODO add load songs
-			
-			userNum ++;
 		}
 		
 		scanner.close();
@@ -60,28 +48,65 @@ public class UserDatabase {
 		if (users.containsKey(username)) {
 			return false;
 		}
-		// add the user name to the list of users
-		User newUser = new User(username, password);
+
+		//generate a salt for the password
+		SecureRandom random = new SecureRandom();
+		byte[] salt = new byte[64];
+		// salt = 64 random bytes
+		random.nextBytes(salt);
+		
+		// encrypt the password
+		String encodedPassword = encode(password, salt);
+		
+		//
+		User newUser = new User(username, encodedPassword);
+		// add the salt to the user
+		newUser.setSalt(salt);
+		// add the user to the hashMap of users
 		users.put(username,newUser);
+		
 		return true;
 	}
 	
-	// TODO add information to a users account
-	
-	// current user login
+	// determine if the entered user name and password match the expected information
 	public User login(String username, String pass) {
+		
 		if (users.containsKey(username)) {
+			// get the users encoded password and salt
 			String password = users.get(username).getPassword();
-			if (password.equals(pass)) {
-				//TODO escaping reference
+			byte[] salt = users.get(username).getSalt();
+			//encode the entered password
+			String encodedPW = encode(pass, salt);
+			// check if the passwords are the same
+			if (password.equals(encodedPW)) {
+				// if they are the same then return the 
 				return users.get(username);
 			}
 		}
-		// if the user does not exist return null
-		// TODO handle the null in the view
 		return null;
 	}
+	
+	// encodes the given string
+	public String encode(String password, byte[] salt) {
+		//
+		try {
+			MessageDigest sha256 = MessageDigest.getInstance("SHA-256");
+			String encodedPassword = "";
+			//add the salt and password to sha256
+			sha256.update(password.getBytes());
+			sha256.update(salt);
+			//hash the password
+			byte[] hash = sha256.digest();
+			//convert the hash into a string
+			for (byte b : hash) {
+				encodedPassword += String.format("%02X", b);
+			}
+			return encodedPassword;
 
-	
-	
+		} catch (NoSuchAlgorithmException e) {
+			System.out.println("Error with encode");
+			return null;
+		}
+	}
+
 }
